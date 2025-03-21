4c3ae28087ce16062ef38a906a2569156cd0b288
mhazad99 <58328915+mhazad99@users.noreply.github.com>
2025-03-21 17:56:06 -0400

    Initial commit

---
+++ modules/ExampleModulesPackage/SignalGenerator/__init__.py
@@ -0,0 +1,9 @@
+"""
+@ Valorisation Recherche HSCM, Societe en Commandite – 2023
+See the file LICENCE for full license details.
+"""
+from .SignalGenerator import SignalGenerator
+from .SignalGeneratorSettingsView import SignalGeneratorSettingsView
+from .SignalGeneratorResultsView import SignalGeneratorResultsView
+from .Ui_SignalGeneratorResultsView import Ui_SignalGeneratorResultsView
+from .Ui_SignalGeneratorSettingsView import Ui_SignalGeneratorSettingsView
