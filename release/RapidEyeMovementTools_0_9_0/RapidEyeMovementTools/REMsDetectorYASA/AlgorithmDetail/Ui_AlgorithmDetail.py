bb5d495915727080873d1cf69f8c8c3041f6b85c
mhazad99 <mhazad1999@gmail.com>
2025-04-07 19:29:13 -0400

    new release added

--- release/RapidEyeMovementTools_0_9_0/RapidEyeMovementTools/REMsDetectorYASA/AlgorithmDetail/Ui_AlgorithmDetail.py
+++ release/RapidEyeMovementTools_0_9_0/RapidEyeMovementTools/REMsDetectorYASA/AlgorithmDetail/Ui_AlgorithmDetail.py
@@ -64,13 +64,12 @@ class Ui_AlgorithmDetail(object):
         self.scrollArea.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
         self.scrollAreaWidgetContents.setObjectName(u"scrollAreaWidgetContents")
-        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 1134, 690))
+        self.scrollAreaWidgetContents.setGeometry(QRect(0, 0, 895, 415))
         self.verticalLayout_5 = QVBoxLayout(self.scrollAreaWidgetContents)
         self.verticalLayout_5.setObjectName(u"verticalLayout_5")
         self.label_2 = QLabel(self.scrollAreaWidgetContents)
         self.label_2.setObjectName(u"label_2")
         self.label_2.setPixmap(QPixmap(u":/image/Eyes_movement.PNG"))
-        self.label_2.setScaledContents(True)
 
         self.verticalLayout_5.addWidget(self.label_2)
 
@@ -101,7 +100,7 @@ class Ui_AlgorithmDetail(object):
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:'Roboto-Regular'; font-size:10pt; font-weight:400; font-style:normal;\">\n"
-"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">This algorithm uses the idea from the methodologies proposed in [1] and [2], primarily building upon Agwal\u2019s [1] approach, which applies amplitude thresholding to the negative product of the LOC and ROC filtered signals. Using this technique, the algorithm identifies signal peaks and extracts key features, including the duration of the REM period, the peak absolute values of ROC and LOC, as well as the absolute rise and fall slopes for both signals.</p>\n"
+"<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\">This algorithm uses the idea from the methodologies proposed in [1] and [2], primarily building upon Agwal\u2019s [2] approach, which applies amplitude thresholding to the negative product of the LOC and ROC filtered signals. Using this technique, the algorithm identifies signal peaks and extracts key features, including the duration of the REM period, the peak absolute values of ROC and LOC, as well as the absolute rise and fall slopes for both signals.</p>\n"
 "<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block"
                         "-indent:0; text-indent:0px;\">To ensure reliable performance, the algorithm requires a minimum of 50 detected REMs to apply its model, which is based on the IsolationForest random forest classifier. Additionally, if the user selects the &quot;remove outlier&quot; option as <span style=\" font-style:italic;\">True</span>, any outliers detected after applying the IsolationForest will be excluded from the final detection results.</p>\n"
 "<p style=\" margin-top:12px; margin-bottom:12px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-weight:600;\">Usage points</span></p>\n"
